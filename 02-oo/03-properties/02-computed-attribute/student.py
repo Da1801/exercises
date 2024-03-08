fc47da25ccb8bef60e4e0918e32cc255da2701bd
Dhruv Agarwal <dhruv.agarwal@student.ucll.be>
2024-03-08 14:59:13 +0100

    Some updates

--- 02-oo/03-properties/02-computed-attribute/student.py
+++ 02-oo/03-properties/02-computed-attribute/student.py
@@ -1,1 +1,17 @@
 # Write your code here
+class BMICalculator:
+    def __init__(self,weight_in_kg,height_in_m):
+        self.weight = weight_in_kg
+        self.height = height_in_m
+    
+    @property
+    def bmi(self):
+        return self.weight / self.height**2
+    @property
+    def category(self):
+        if self.bmi < 18.5:
+           return "underweight"
+        elif 18.5 <self.bmi < 25:
+            return "normal"
+        else:
+            return "overweight"
